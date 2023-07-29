eeb8798866426cdf0c2e28002af45a97eb1e8471
amit dubey <amitdubey6261@gmail.com>
2023-07-29 16:38:00 +0530

    complete-joystick

--- Client/src/Experience/Controllers.ts
+++ Client/src/Experience/Controllers.ts
@@ -1,6 +1,6 @@
-import * as THREE from 'three' ; 
+import * as THREE from 'three';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js' ; 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import nipplejs from 'nipplejs';
 
 import Experience from "./Experience";
@@ -9,58 +9,122 @@ import Camera from './Camera';
 import Renderer from './Renderer';
 import { EventEmitter } from 'events';
 
-class Controllers extends EventEmitter{
-    experience : Experience ; 
-    scene : THREE.Scene ; 
-    canvas : HTMLCanvasElement ;
-    sizes : Sizes ; 
-    renderer : Renderer ; 
-    camera : Camera ; 
-    orbitControls : OrbitControls ; 
+class Controllers extends EventEmitter {
+    experience: Experience;
+    scene: THREE.Scene;
+    canvas: HTMLCanvasElement;
+    sizes: Sizes;
+    renderer: Renderer;
+    camera: Camera;
+    orbitControls: OrbitControls;
 
-    constructor(){
+    fwdValue: number;
+    bkdValue: number;
+    lftValue: number;
+    rgtValue: number;
+    tempVector: THREE.Vector3;
+    upVector: THREE.Vector3;
+
+    constructor() {
         super();
-        this.experience = new Experience() ; 
-        this.scene = this.experience.scene ; 
-        this.canvas = this.experience.canvas ; 
-        this.camera = this.experience.camera  ; 
-        this.renderer = this.experience.renderer ; 
-        this.sizes = this.experience.sizes ; 
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+        this.canvas = this.experience.canvas;
+        this.camera = this.experience.camera;
+        this.renderer = this.experience.renderer;
+        this.sizes = this.experience.sizes;
 
-        this.createControlls() ; 
-    }
-    
-    createControlls(){
-        this.createOrbitControlls() ; 
-        this.createjoyStick() ; 
+        this.createControlls();
     }
 
-    createjoyStick(){
+    createControlls() {
+        this.createOrbitControlls();
+        this.createjoyStick();
+    }
+
+    createjoyStick() {
+        this.tempVector = new THREE.Vector3() ; 
+        this.upVector = new THREE.Vector3( 0 , 1 , 0 );
+
+        //left joystick
+
         const manager = nipplejs.create({
-            zone : document.getElementById('left-joyStick') as HTMLElement ,
-            color : 'red' , 
-            position : {
-                top : '50%' , 
-                left : '50%' ,
-            } ,
-            mode : 'static' , 
+            zone: document.getElementById('left-joyStick') as HTMLElement,
+            color: 'red',
+            position: {
+                top: '50%',
+                left: '50%',
+            },
+            mode: 'static',
         })
 
-        manager.on('move' , (e:any , nipple)=>{
-            this.emit('leftMove' , nipple ) ; 
+        manager.on('move', ( _ , nipple) => {
+            this.emit('leftMove', nipple);
         })
+
+        //right joy stick
+
+        const manager2 = nipplejs.create({
+            zone: document.getElementById('right-joyStick') as HTMLElement,
+            color: 'red',
+            position: {
+                top: '50%',
+                left: '50%',
+            },
+            mode: 'static',
+        })
+
+        manager2.on('move', (_, data)=>{
+            const forward = data.vector.y
+            const turn = data.vector.x
+
+            if (forward > 0) {
+                this.fwdValue = Math.abs(forward) ; 
+                this.bkdValue = 0 ; 
+            } else if (forward < 0) {
+                this.fwdValue = 0 ; 
+                this.bkdValue = Math.abs(forward) ; 
+            }
+
+            if (turn > 0) {
+                this.lftValue = 0 ; 
+                this.rgtValue = Math.abs(turn) ; 
+            } else if (turn < 0) {
+                this.lftValue = Math.abs(turn) ; 
+                this.rgtValue = 0 ; 
+            }
+        })
+
+        manager2.on('end', ()=>{
+            this.bkdValue = 0
+            this.fwdValue = 0
+            this.lftValue = 0
+            this.rgtValue = 0
+        })
+
     }
 
-    createOrbitControlls(){
-        this.orbitControls = new OrbitControls( this.camera.perspectiveCamera , this.canvas);
-        this.orbitControls.enableDamping = true ; 
-        this.orbitControls.enableZoom = true ; 
-        this.orbitControls.enableRotate = true ; 
+    createOrbitControlls() {
+        this.orbitControls = new OrbitControls(this.camera.perspectiveCamera, this.experience.canvas);
+        this.orbitControls.maxDistance = 10;
+        this.orbitControls.minDistance = 10;
+        // this.orbitControls.maxPolarAngle = (Math.PI / 4) * 3;
+        this.orbitControls.maxPolarAngle = Math.PI / 2;
+        this.orbitControls.minPolarAngle = 0;
+        this.orbitControls.autoRotate = false;
+        this.orbitControls.autoRotateSpeed = 0;
+        this.orbitControls.rotateSpeed = 0.5;
+        this.orbitControls.enableDamping = false;
+        this.orbitControls.dampingFactor = 0.1;
+        this.orbitControls.enableZoom = false;
+        this.orbitControls.enablePan = false;
+        // this.orbitControls.minAzimuthAngle = - Math.PI / 2; // radians
+        // this.orbitControls.maxAzimuthAngle = Math.PI / 4 // radians
     }
 
-    update(){
+    update() {
         this.orbitControls.update();
     }
 }
 
-export default Controllers ; 
+export default Controllers; 
--- Client/src/Experience/World/World.ts
+++ Client/src/Experience/World/World.ts
@@ -1,20 +1,23 @@
 import * as THREE from 'three' ;
 import Experience from "../Experience";
 import Resources from '../Utils/Resources';
-import { mod } from 'three/examples/jsm/nodes/Nodes.js';
+import Controllers from '../Controllers';
 
 
 class World{
     experience : Experience ;
-    controller : any ; 
+    controller : Controllers ; 
     scene : THREE.Scene ; 
     resources : Resources ; 
-    model : any  ; 
+    model : THREE.Scene  ; 
+    camera : THREE.PerspectiveCamera ; 
 
     constructor(){
-        this.experience = new Experience() ; 
+       this.experience = new Experience() ; 
+       this.camera = this.experience.camera.perspectiveCamera ; 
         this.scene = this.experience.scene ; 
         this.resources = this.experience.resources ; 
+        this.controller = this.experience.controllers ; 
 
         this.setModel() ; 
     }
@@ -29,6 +32,40 @@ class World{
     }
 
     update(){
+        this.updatePlayer();
+    }
+
+    updatePlayer(){
+        const angle = this.controller.orbitControls.getAzimuthalAngle() ; 
+        if(this.controller.fwdValue != undefined ){
+
+            if( this.controller.fwdValue > 0 ){
+                this.controller.tempVector.set( 0 , 0 , -this.controller.fwdValue ).applyAxisAngle(this.controller.upVector , angle ) ; 
+                this.model.position.addScaledVector(this.controller.tempVector , 1);
+            }
+
+            if( this.controller.bkdValue > 0 ){
+                this.controller.tempVector.set( 0 , 0 , this.controller.bkdValue ).applyAxisAngle(this.controller.upVector , angle) ;
+                this.model.position.addScaledVector(this.controller.tempVector , 1 );  
+            }
+
+            if( this.controller.lftValue > 0 ){
+                this.controller.tempVector.set( -this.controller.lftValue , 0 , 0 ).applyAxisAngle(this.controller.upVector , angle) ;
+                this.model.position.addScaledVector(this.controller.tempVector , 1 );  
+            }
+
+            if( this.controller.rgtValue > 0 ){
+                this.controller.tempVector.set( this.controller.rgtValue , 0 , 0 ).applyAxisAngle(this.controller.upVector , angle) ;
+                this.model.position.addScaledVector(this.controller.tempVector , 1 );  
+            }
+
+            this.model.updateMatrixWorld() ; 
+
+            this.camera.position.sub(this.controller.orbitControls.target); 
+            this.controller.orbitControls.target.copy(this.model.position);
+            this.camera.position.add(this.model.position);
+
+        } 
     }
 }
 
